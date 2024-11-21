f1120ed635a12abbc900cf3ef60417f446fcb27a
AlanBuell <frog13cha1r@gmail.com>
2024-11-21 15:02:14 -0800

    PlayerMovement Script

---
+++ Final/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Name: Alan Buell
+ * Date: 11/21/2024
+ * Des: Handles player movement moving with the A, D, left arrow and right arrow keys.
+ * also handles collsion/trigger enters for walls (invisible), doors, and the dateable character at the end. 
+ */
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 10;
+
+    private Vector3 moveDir;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();  
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A | KeyCode.LeftArrow))
+        {
+            moveDir = Vector3.left;
+            rb.MovePosition(transform.position + moveDir * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D | KeyCode.RightArrow))
+        {
+            moveDir = Vector3.right;
+            rb.MovePosition(transform.position + moveDir * moveSpeed * Time.deltaTime);
+        }
+
+            
+    }
+}
