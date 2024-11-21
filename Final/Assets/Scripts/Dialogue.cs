a6a86e6ae7375497b3d28cc0b27c1fbc47173e90
Vex <Vexvasquez@gmail.com>
2024-11-21 15:46:05 -0800

    Final Push for 11/21/24

---
+++ Final/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * VEX VASQUEZ
+ * 11/21/2024
+ * Manages Dialogue between characters
+ */
+public class Dialogue : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
