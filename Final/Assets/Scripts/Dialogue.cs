a03dacb9eda8e4b882deed44e4b0c046aca35be0
Vex <Vexvasquez@gmail.com>
2024-12-04 14:40:34 -0800

    DialogueCont

--- Final/Assets/Scripts/Dialogue.cs
+++ Final/Assets/Scripts/Dialogue.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting.FullSerializer;
 
 /*
  * VEX VASQUEZ
@@ -15,62 +16,4 @@ public class Dialogue : ScriptableObject
 {
     public DialogueNode RootNode;
 
-    /*
-    public TextMeshProUGUI textComponent;
-    public string[] lines;
-    public float textSpeed;
-
-    private int index;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        textComponent.text = string.Empty;
-        StartDialogue();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
-        {
-            if (textComponent.text == lines[index])
-            {
-                NextLine();
-            }
-            else
-            {
-                StopAllCoroutines();
-                textComponent.text = lines[index];
-            }
-        }
-    }
-
-    
-    void StartDialogue()
-    {
-        index = 0;
-        StartCoroutine(TypeLine());
-    }
-
-    IEnumerator TypeLine()
-    {
-        foreach (char c in lines[index].ToCharArray())
-        {
-            textComponent.text += c;
-            yield return new WaitForSeconds(textSpeed);
-        }
-    }
-
-    void NextLine()
-    {
-        if (index < lines.Length - 1)
-        {
-            index++;
-            textComponent.text = string.Empty;
-            StartCoroutine(TypeLine());
-        }
-    }
-    */
 }
