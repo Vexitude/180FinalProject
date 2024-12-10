550f235c9f754ae2beb5bbd275ff547062707dd3
AlanBuell <frog13cha1r@gmail.com>
2024-12-10 14:56:54 -0800

    Jenny Blink

--- Final/Assets/Scripts/Jenny.cs
+++ Final/Assets/Scripts/Jenny.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Name: Alan Buell
+ * Date: 12/10/024
+ * Des: Allows Jenny (dateable character) to blink, working on her reactions
+ */
 public class Jenny : MonoBehaviour
 {
+    public GameObject JennyOpen;
+    public GameObject JennyClosed;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("JennyBlink", 0.5f, 0.5f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
+    }
+    private void JennyBlink()
+    {
+        int randNum = Random.Range(0, 2);
+
+        if(randNum == 0)
+        {
+            Instantiate(JennyOpen,transform.position, Quaternion.identity);
+        }
+        else if (randNum == 1)
+        {
+            Instantiate(JennyClosed, transform.position, Quaternion.identity);
+        }
     }
 }
